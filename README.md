39adff760a5cee3ab4eaf59ac654885b6ab09759
andrelohmann <lohmann.andre@gmail.com>
2014-08-15 19:10:47 +0200

    First checkin to new repository after rebuild to composer usage

---
+++ README.md
@@ -0,0 +1,10 @@
+# localegeoip
+
+## Maintainers
+
+ * Andre Lohmann (Nickname: andrelohmann)
+  <lohmann dot andre at googlemail dot com>
+
+## Introduction
+
+Map GeoIPs to locales or vice versa
---
+++ _config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Module is dependent on Geoip
+ * 
+ * 
+ * apt-get install geoip-bin php5-geoip
+ * 
+ * set default Locales
+ * Geoip::set_default_country_code('US');
+ * LocaleGeoip::set_default_locale_code('en_US');
+ * 
+ */
